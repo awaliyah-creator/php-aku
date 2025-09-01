3ed5934df274508d8a503ca092d486334bba398d
Alifhia Awaliyah <alifhiaawaliyah@gmail.com>
2025-09-01 11:24:37 +0700

    tambahkan about & edit index

--- about.php
+++ about.php
@@ -3,12 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP DASAR</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php $nama = "Alifhia";
-        $umur = 16;
-    ?>
-    <h1>Selamat Datang,<?php echo $nama;  $umur; ?>!</h1>
+    Ini Halaman About
 </body>
 </html>
